67f972e04217ef207c990011fcb04429f2465f0f
Gourav Mittal <gouravmittal@outlook.in>
2018-03-30 13:24:20 +0530

    Add post Todos route and refactor models

--- server/server.js
+++ server/server.js
@@ -1,5 +1,27 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// save new something
+var port = 3000;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    var todo = new Todo({
+        text: request.body.text
+    });
+
+    todo.save().then((doc) => {
+        response.send(doc);
+    }, (e) => {
+        response.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port: ${port}`);
+})
